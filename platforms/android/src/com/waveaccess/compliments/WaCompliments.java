9b17b73e5d5281ac735a8cf984444c1c9419cb3f
Maksim.Kanev <maksim.kanev@gmail.com>
2014-03-05 16:26:17 +0400

    RC1

--- platforms/android/src/com/waveaccess/compliments/WaCompliments.java
+++ platforms/android/src/com/waveaccess/compliments/WaCompliments.java
@@ -19,19 +19,81 @@
 
 package com.waveaccess.compliments;
 
-import android.os.Bundle;
-import org.apache.cordova.*;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
-public class WaCompliments extends CordovaActivity 
-{
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import org.apache.cordova.Config;
+import org.apache.cordova.CordovaActivity;
+
+public class WaCompliments extends CordovaActivity {
+
+    /**
+     * The view to show the ad.
+     */
+    private AdView adView;
+
+    /* Your ad unit id. Replace with your actual ad unit id. */
+    private static final String AD_UNIT_ID = "a15317047e21026";
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.init();
         // Set by <content src="index.html" /> in config.xml
         super.loadUrl(Config.getStartUrl());
         //super.loadUrl("file:///android_asset/www/index.html")
+        // Create an ad.
+        adView = new AdView(this);
+        adView.setAdSize(AdSize.SMART_BANNER);
+        adView.setAdUnitId(AD_UNIT_ID);
+        // Add the AdView to the view hierarchy. The view will have no size
+        // until the ad is loaded.
+        super.root.addView(adView);
+
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device.
+        AdRequest adRequest = new AdRequest.Builder()
+            .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+            .addTestDevice("9591A87932AA4DDF6E6C0E740BFEB9E0")
+            .build();
+
+        // Start loading the ad in the background.
+        adView.loadAd(adRequest);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (adView != null) {
+            adView.resume();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        if (adView != null) {
+            adView.pause();
+        }
+        super.onPause();
+    }
+
+    /**
+     * Called before the activity is destroyed.
+     */
+    @Override
+    public void onDestroy() {
+        // Destroy the AdView.
+        if (adView != null) {
+            adView.destroy();
+        }
+        super.onDestroy();
     }
 }
 
