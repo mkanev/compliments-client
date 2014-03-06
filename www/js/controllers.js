33942b58361710ca6a55bb0bcd4203e667e6bbf6
Maksim.Kanev <maksim.kanev@gmail.com>
2014-03-06 13:08:52 +0400

    REST reworked

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,24 +3,16 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('HomeCtrl', function ($scope, API) {
-                var emptyEntities = [
-                  {content: 'Потяните вниз, чтобы обновить'}
-                ];
-                $scope.criteria = {
-                  page: Math.floor((Math.random() * 1000) + 1),
-                  limit: 1
-                };
-                $scope.entities = emptyEntities;
+  .controller('HomeCtrl', function ($scope, Restangular) {
+                var emptyMsg = $scope.entity = {content: 'Потяните вниз, чтобы обновить'};
                 $scope.fetchResult = function () {
                   // Show the loading overlay and text
-                  return API.compliment.getRecords($scope.criteria).then(function (data) {
-                    $scope.entities = data;
+                  return Restangular.all('compliment').customGET('random').then(function (data) {
+                    $scope.entity = data;
                     delete $scope.error;
-                    $scope.criteria.page = Math.floor(Math.random() * data.entitiesCount);
                   }, function (response) {
                     $scope.error = 'Для корректной работы приложения необходимо подключение к сети Интернет';
-                    $scope.entities = emptyEntities;
+                    $scope.entity = emptyMsg;
                   });
                 };
                 $scope.onRefresh = function () {
