c65dd6936feb0e31cf73e5d8c1908f37bbb32529
Maksim.Kanev <maksim.kanev@gmail.com>
2014-03-17 17:08:59 +0400

    Share supported

--- platforms/android/src/com/waveaccess/compliments/plugin/SharePlugin.java
+++ platforms/android/src/com/waveaccess/compliments/plugin/SharePlugin.java
@@ -5,7 +5,6 @@ import com.google.analytics.tracking.android.MapBuilder;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -15,27 +14,26 @@ import org.json.JSONException;
 /**
  * @author <a href="mailto:maksim.kanev@waveaccess.ru">Maksim Kanev</a>
  */
-public class Sender extends CordovaPlugin {
+public class SharePlugin extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (!"sms".equals(action)) {
-            return false;
-        }
-        String message = args.getString(0);
-        String number = args.getString(1);
-        this.sendSMS(number, message);
-        return true;
+        return "share".equals(action) && share(args.getString(0));
+
     }
 
-    private void sendSMS(String phoneNumber, String message) {
+    private boolean share(String message) {
         Activity activity = cordova.getActivity();
-        activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + phoneNumber + "?body=" + message)));
+        Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        sharingIntent.putExtra(Intent.EXTRA_TEXT, message);
+        activity.startActivity(Intent.createChooser(sharingIntent, "Share via"));
         EasyTracker.getInstance(activity).send(MapBuilder.createEvent(
             "share",     // Event category (required)
             "short_message_send",  // Event action (required)
             "sms_btn",   // Event label
             null
         ).build());
+        return true;
     }
 }
