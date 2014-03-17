49d62bde9bd6e2db45dffdffce0fc454aee3864b
Maksim.Kanev <maksim.kanev@gmail.com>
2014-03-17 16:27:00 +0400

    SMS Plugin now tracks the event

--- platforms/android/src/com/waveaccess/compliments/plugin/Sender.java
+++ platforms/android/src/com/waveaccess/compliments/plugin/Sender.java
@@ -1,5 +1,9 @@
 package com.waveaccess.compliments.plugin;
 
+import com.google.analytics.tracking.android.EasyTracker;
+import com.google.analytics.tracking.android.MapBuilder;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 
@@ -15,16 +19,23 @@ public class Sender extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if ("sms".equals(action)) {
-            String message = args.getString(0);
-            String number = args.getString(1);
-            this.sendSMS(number, message);
-            return true;
+        if (!"sms".equals(action)) {
+            return false;
         }
-        return false;
+        String message = args.getString(0);
+        String number = args.getString(1);
+        this.sendSMS(number, message);
+        return true;
     }
 
     private void sendSMS(String phoneNumber, String message) {
-        cordova.getActivity().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + phoneNumber + "?body=" + message)));
+        Activity activity = cordova.getActivity();
+        activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + phoneNumber + "?body=" + message)));
+        EasyTracker.getInstance(activity).send(MapBuilder.createEvent(
+            "share",     // Event category (required)
+            "short_message_send",  // Event action (required)
+            "sms_btn",   // Event label
+            null
+        ).build());
     }
 }
